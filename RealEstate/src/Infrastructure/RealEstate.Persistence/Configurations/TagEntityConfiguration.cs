759366efaad94422b0d70a96917794d7fe25bb38
isnuraina <nurane.ismayilzade19@gmail.com>
2025-07-15 16:04:18 +0400

    Tag entity and tag entity configuration is ready!

---
+++ RealEstate/src/Infrastructure/RealEstate.Persistence/Configurations/TagEntityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RealEstate.Domain.Entities;
+using RealEstate.Infrastructure.Extensions;
+
+namespace RealEstate.Persistence.Configurations
+{
+    internal class TagEntityConfiguration : IEntityTypeConfiguration<Tag>
+    {
+        public void Configure(EntityTypeBuilder<Tag> builder)
+        {
+            builder.Property(m => m.Id).HasColumnType("int");
+            builder.Property(m => m.Name).HasColumnType("nvarchar").HasMaxLength(100).IsRequired();
+            builder.ConfigureAuditable();
+            builder.HasKey(m => m.Id);
+            builder.ToTable("Tags");
+        }
+    }
+}
