99ac94f354a76da8f70167ea0309897c67568141
isnuraina <nurane.ismayilzade19@gmail.com>
2025-07-15 19:58:00 +0400

    Some entities and some configurations is ready!

---
+++ RealEstate/src/Infrastructure/RealEstate.Persistence/Configurations/BlogPostCommentConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RealEstate.Domain.Entities;
+using RealEstate.Infrastructure.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate.Persistence.Configurations
+{
+    internal class BlogPostCommentConfiguration : IEntityTypeConfiguration<BlogPostComment>
+    {
+        public void Configure(EntityTypeBuilder<BlogPostComment> builder)
+        {
+            builder.Property(m => m.Id).HasColumnType("int");
+            builder.Property(m => m.Text).HasColumnType("nvarchar(max)").IsRequired();
+            builder.Property(m => m.BlogPostId).HasColumnType("int").IsRequired();
+            builder.Property(m => m.ParentId).HasColumnType("int");
+            builder.ConfigureAuditable();
+            builder.HasKey(m => m.Id);
+            builder.ToTable("BlogPostComments");
+
+            builder.HasOne<BlogPost>()
+                .WithMany()
+                .HasForeignKey(m => m.BlogPostId)
+                .HasPrincipalKey(m => m.Id)
+                .OnDelete(DeleteBehavior.NoAction);
+
+
+            builder.HasOne<BlogPostComment>()
+               .WithMany()
+               .HasForeignKey(m => m.ParentId)
+               .HasPrincipalKey(m => m.Id)
+               .OnDelete(DeleteBehavior.NoAction);
+
+        }
+    }
+}
---
+++ RealEstate/src/Infrastructure/RealEstate.Persistence/Configurations/BlogPostTagEntityConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RealEstate.Domain.Entities;
+using RealEstate.Infrastructure.Extensions;
+
+namespace RealEstate.Persistence.Configurations
+{
+    internal class BlogPostTagEntityConfiguration : IEntityTypeConfiguration<BlogPostTag>
+    {
+        public void Configure(EntityTypeBuilder<BlogPostTag> builder)
+        {
+            builder.Property(m => m.BlogPostId).HasColumnType("int");
+            builder.Property(m => m.TagId).HasColumnType("int");
+            builder.ConfigureAuditable();
+            builder.HasKey(m => new { m.BlogPostId, m.TagId });
+            builder.ToTable("BlogPostTags");
+
+
+            builder.HasOne<BlogPost>()
+                .WithMany()
+                .HasForeignKey(m => m.BlogPostId)
+                .HasPrincipalKey(m => m.Id)
+                .OnDelete(DeleteBehavior.NoAction);
+
+
+
+            builder.HasOne<Tag>()
+                .WithMany()
+                .HasForeignKey(m => m.TagId)
+                .HasPrincipalKey(m => m.Id)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
